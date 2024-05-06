84a4639f0a4c4f23442fd860d2c11fe92b91f18f
nini0309 <102242027+nini0309@users.noreply.github.com>
2024-05-06 16:49:43 +0530

    pushed project

---
+++ src/modul/dish.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+const dish=mongoose.Schema({
+    dname:String,
+    dtype:String,
+    dprice:Number,
+    dtime:String,
+    
+    photo:String,
+    discription:String,
+    ddiscount:Number,
+    dserve:Number
+})
+module.exports=mongoose.model("dish",dish)
